6e5f0288343f951cb57ef6b09d84f2d69dcca00a
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-28 17:44:17 -0500

    Added endpoints to controllers.

--- TimedAssignment.WebAPI/Controllers/UserController.cs
+++ TimedAssignment.WebAPI/Controllers/UserController.cs
@@ -11,6 +11,26 @@ namespace TimedAssignment.WebAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        
+        private readonly IUserService _userService;
+        private readonly ITokenService _tokenService;
+
+        public UserController(IUserService userService, ITokenService tokenService)
+        {   
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost ("SignUp")]
+        public async Task<IActionResult> SignUp([FromBody] SignUpUser model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var signUpResult = await _userService.SignUpUserAsync(model0);
+            if (signUpResult)
+                return Ok("You have successfully signed up!");
+
+            return BadRequest("You could not be signed up.");
+        }
     }
 }
