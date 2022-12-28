3fef1e65e16b66178b83f6702554872db15d0adb
Darneisha Miller <darneisham20@gmail.com>
2022-12-27 21:31:57 -0500

    Changes w/ Terry + Commenting out Services

--- TimedAssignment.Services/Post/PostService.cs
+++ TimedAssignment.Services/Post/PostService.cs
@@ -6,16 +6,16 @@ using Microsoft.AspNetCore.Http;
 
 namespace TimedAssignment.Services.Post
 {
-    private readonly int _userId;
     public class PostService : IPostService
     {
-        public PostService(IHttpContextAccessor httpContextAccessor)
-        {
-            var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
-            var value = userClaims.FindFirst("Id")?.Value;
-            var validId = int.TryParse(value, out _userId);
-            if (!validId)
-                throw new Exception("Attempted to build PostService without UserId claim.");
-        }
+    private readonly int _userId;
+        // public PostService(IHttpContextAccessor httpContextAccessor)
+        // {
+        //     var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
+        //     var value = userClaims.FindFirst("Id")?.Value;
+        //     var validId = int.TryParse(value, out _userId);
+        //     if (!validId)
+        //         throw new Exception("Attempted to build PostService without UserId claim.");
+        // }
     }
 }
