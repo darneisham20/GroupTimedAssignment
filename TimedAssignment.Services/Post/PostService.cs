39344d43b96a9ef77dcfcd0e01694a6c5d5f4ea9
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-27 20:56:24 -0500

    Added code to the Services and created and added APIController starts to the Controllers

--- TimedAssignment.Services/Post/PostService.cs
+++ TimedAssignment.Services/Post/PostService.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace TimedAssignment.Services.Post
 {
+    private readonly int _userId;
     public class PostService : IPostService
     {
-        
+        public PostService(IHttpContextAccessor httpContextAccessor)
+        {
+            var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
+            var value = userClaims.FindFirst("Id")?.Value;
+            var validId = int.TryParse(value, out _userId);
+            if (!validId)
+                throw new Exception("Attempted to build PostService without UserId claim.");
+        }
     }
 }
