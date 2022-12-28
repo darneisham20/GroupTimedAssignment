e03e0f087a63c06f5afe991c4bd9df328b4fd3c6
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-28 18:35:03 -0500

    last sad attempt to add functionality.

--- TimedAssignment.WebAPI/Controllers/UserController.cs
+++ TimedAssignment.WebAPI/Controllers/UserController.cs
@@ -1,36 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Http;
 
-namespace TimedAssignment.WebAPI.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
-        private readonly IUserService _userService;
-        private readonly ITokenService _tokenService;
+// namespace TimedAssignment.WebAPI.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class UserController : ControllerBase
+//     {
+//         private readonly IUserService _userService;
+//         private readonly ITokenService _tokenService;
 
-        public UserController(IUserService userService, ITokenService tokenService)
-        {   
-            _userService = userService;
-            _tokenService = tokenService;
-        }
+//         public UserController(IUserService userService, ITokenService tokenService)
+//         {   
+//             _userService = userService;
+//             _tokenService = tokenService;
+//         }
 
-        [HttpPost ("SignUp")]
-        public async Task<IActionResult> SignUp([FromBody] SignUpUser model)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+//         [HttpPost ("SignUp")]
+//         public async Task<IActionResult> SignUp([FromBody] SignUpUser model)
+//         {
+//             if (!ModelState.IsValid)
+//                 return BadRequest(ModelState);
 
-            var signUpResult = await _userService.SignUpUserAsync(model0);
-            if (signUpResult)
-                return Ok("You have successfully signed up!");
+//             var signUpResult = await _userService.SignUpUserAsync(model0);
+//             if (signUpResult)
+//                 return Ok("You have successfully signed up!");
 
-            return BadRequest("You could not be signed up.");
-        }
-    }
-}
+//             return BadRequest("You could not be signed up.");
+//         }
+//     }
+// }
