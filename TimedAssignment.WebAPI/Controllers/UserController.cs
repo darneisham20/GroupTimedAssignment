2d862621bef4d62bce94810fcacbb7a85c7eb473
Darneisha Miller <darneisham20@gmail.com>
2022-12-28 18:23:14 -0500

    Refactors changed in UserController

--- TimedAssignment.WebAPI/Controllers/UserController.cs
+++ TimedAssignment.WebAPI/Controllers/UserController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using TimedAssignment.Services.User;
+using TimedAssignment.Services.Token;
 
 namespace TimedAssignment.WebAPI.Controllers
 {
