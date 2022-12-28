900ac0e4302319e3196f1bcb2f4868dd26e74dc2
jamiecoakley <jamiemcoakley@gmail.com>
2022-12-27 21:51:21 -0500

    Made some database fixes with Terry.

--- TimedAssignment.Services/Post/PostService.cs
+++ TimedAssignment.Services/Post/PostService.cs
@@ -5,17 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace TimedAssignment.Services.Post
-{
-    private readonly int _userId;
+{    
     public class PostService : IPostService
     {
-        public PostService(IHttpContextAccessor httpContextAccessor)
-        {
-            var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
-            var value = userClaims.FindFirst("Id")?.Value;
-            var validId = int.TryParse(value, out _userId);
-            if (!validId)
-                throw new Exception("Attempted to build PostService without UserId claim.");
-        }
+        private readonly int _userId;
+        // public PostService(IHttpContextAccessor httpContextAccessor)
+        // {
+        //     var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
+        //     var value = userClaims.FindFirst("Id")?.Value;
+        //     var validId = int.TryParse(value, out _userId);
+        //     if (!validId)
+        //         throw new Exception("Attempted to build PostService without UserId claim.");
+        // }
     }
 }
